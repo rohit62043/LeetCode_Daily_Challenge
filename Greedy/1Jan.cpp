69ca37771920b76befe938e62c05c12a86104e56
Rohit Kumar Singh <100895939+rohit62043@users.noreply.github.com>
2024-01-02 08:13:19 +0000

    1Jan Done

---
+++ Greedy/1Jan.cpp
@@ -0,0 +1,18 @@
+// Assign Cookies
+ int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int count=0;
+        int i=0;
+        int j=0;
+
+        while(i<g.size() && j<s.size()){
+            if(g[i]<=s[j]){
+                    count++;
+                    i++;
+            }
+            j++;
+        }
+        
+        return count;
+    }
